d58134356115312946698584bafaca0a3ac7d7a7
ewalker41 <ewalker@factual.com>
2013-08-09 13:22:04 -0700

    add website extractor

--- needle.js
+++ needle.js
@@ -1,11 +1,13 @@
 var extracted_address;
 var extracted_tel;
+var extracted_website;
 var src_url;
 
 function getSelectionHtml(string,url) {
   console.log(string);
   var address_pattern = /(\d+\s+[':.,\(\)\s\w]*,\s*[A-Za-z]+\s*\d{5}(-\d{4})?)/i;
-  var tel_pattern = /([\(\)0-9]{3,5}[\. -]*[0-9]{3}[\. -]*[0-9]{4}\b)/i;
+  var tel_pattern = /([\(\)0-9]{3,5}[\. \-\/]*[0-9]{3}[\. \-\/]*[0-9]{4})/i;
+  var website_pattern = /((?:https?:\/\/)?(?:[a-z]+\.)?[a-z0-9\-]+\.(?:com|org|net))/i;
   src_url = url;
 
   match_address = address_pattern.exec(string)
@@ -23,7 +25,14 @@ function getSelectionHtml(string,url) {
   else {
     extracted_tel = "";
   }
-  
+
+  match_website = website_pattern.exec(string)
+  if (match_website) {
+    extracted_website = match_website[1];
+  }
+  else {
+    extracted_website = "";  
+  }
 }
 
 function autoExtractName() {
@@ -50,7 +59,7 @@ chrome.contextMenus.create({
   'onclick': function(info, tab) { 
     getSelectionHtml(info.selectionText,info.pageUrl); 
     chrome.tabs.create({url: 'http://www.factual.com/submit-form/t/places/new'}, function(tab) {
-      chrome.tabs.executeScript(tab.id, {code: "document.getElementById('tel').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('tel').setAttribute('name', 'tel'); document.getElementById('address').setAttribute('name', 'address'); document.getElementById('address').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('country').setAttribute('name', 'country'); document.getElementById('country').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('tel').value = '"+extracted_tel+"'; document.getElementById('address').value = '"+extracted_address+"'; document.getElementById('country').value = 'us'; document.getElementById('submit_form_reference').value = '"+src_url+"'"});
+      chrome.tabs.executeScript(tab.id, {code: "document.getElementById('tel').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('tel').setAttribute('name', 'tel'); document.getElementById('address').setAttribute('name', 'address'); document.getElementById('address').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('country').setAttribute('name', 'country'); document.getElementById('country').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('website').setAttribute('name', 'website'); document.getElementById('website').setAttribute('class', 'check-dirty dirty-field'); document.getElementById('tel').value = '"+extracted_tel+"'; document.getElementById('address').value = '"+extracted_address+"'; document.getElementById('country').value = 'us'; document.getElementById('website').value = '"+extracted_website+"'; document.getElementById('submit_form_reference').value = '"+src_url+"'"});
       });
   }
 });
